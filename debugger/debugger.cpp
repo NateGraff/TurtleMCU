3b8d4353c4bfeccae93862a2daf5b18fda3e15f9
Nathaniel Graff <nathaniel.graff@gmail.com>
2017-12-21 14:14:34 -0800

    Show PC and SP in red and yellow
    Separate function to render screen

--- debugger/debugger.cpp
+++ debugger/debugger.cpp
@@ -15,6 +15,15 @@
 
 using namespace std;
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
 vluint64_t main_time = 0;
 
 double sc_time_stamp() {
@@ -39,6 +48,52 @@ void getRAM(Vcpu * cpu, uint16_t * ram) {
 
 uint16_t getSP(Vcpu * cpu) {
 	return cpu->__PVT__cpu->__PVT__sp_i->__PVT__dout;
+}
+
+void renderScreen(Vcpu * cpu, uint16_t ram_window_start, uint16_t ram_window_end) {
+	// clear terminal
+	printf("\033[2J\033[1;1H");
+
+	// print registers
+	printf("---------------------------------------\n");
+	printf("Registers\n");
+	printf("---------------------------------------\n");
+	printf(KRED "pc" KNRM " = %03x, " KYEL "sp" KNRM " = %03x\n", getPC(cpu), getSP(cpu));
+	
+	uint16_t registers[8];
+	getRegisters(cpu, registers);
+	
+	for(int i = 0; i < 4; i++) {
+		printf("r%d = %04x ", i, registers[i]);
+	}
+	printf("\n");
+	for(int i = 0; i < 4; i++) {
+		printf("r%d = %04x ", i+4, registers[4+i]);
+	}
+	printf("\n");
+
+	// print ram
+	printf("---------------------------------------\n");
+	printf("RAM\n");
+	printf("---------------------------------------\n");
+	uint16_t ram[1024];
+	getRAM(cpu, ram);
+
+	for(uint16_t ram_index = ram_window_start; (ram_index < ram_window_end && (ram_index + 7) < 1024); ram_index += 8) {
+		printf("%03x: ", ram_index);
+		for(int i = 0; i < 8; i++) {
+			if(getPC(cpu) == ram_index + i) {
+				printf(KRED "%04x" KNRM, ram[ram_index + i]);
+			} else if(getSP(cpu) == ram_index + i) {
+				printf(KYEL "%04x" KNRM, ram[ram_index + i]);
+			} else {
+				printf("%04x", ram[ram_index + i]);
+			}
+		}
+		printf("\n");
+	}
+
+	printf("---------------------------------------\n");
 }
 
 int main(int argc, char ** argv) {
@@ -70,6 +125,7 @@ int main(int argc, char ** argv) {
 	cpu->rst_n = 1;
 
 	int quit = 0;
+	int next = 0;
 	uint16_t ram_window_start = 0x000;
 	uint16_t ram_window_end = 0x80;
 
@@ -82,61 +138,33 @@ int main(int argc, char ** argv) {
 		cpu->eval();
 
 		if(clk == 1 && getState(cpu) == 0b011) {
-			// clear terminal
-			printf("\033[2J\033[1;1H");
+			next = 0;
+			while(!next && !quit) {
+				renderScreen(cpu, ram_window_start, ram_window_end);
 
-			// print registers
-			printf("---------------------------------------\n");
-			printf("Registers\n");
-			printf("---------------------------------------\n");
-			printf("pc = %03x, sp = %03x\n", getPC(cpu), getSP(cpu));
-			
-			uint16_t registers[8];
-			getRegisters(cpu, registers);
-			
-			for(int i = 0; i < 4; i++) {
-				printf("r%d = %04x ", i, registers[i]);
-			}
-			printf("\n");
-			for(int i = 0; i < 4; i++) {
-				printf("r%d = %04x ", i+4, registers[4+i]);
-			}
-			printf("\n");
+				printf("(n)ext, (q)uit, move to ram offset: ");
 
-			// print ram
-			printf("---------------------------------------\n");
-			printf("RAM\n");
-			printf("---------------------------------------\n");
-			uint16_t ram[1024];
-			getRAM(cpu, ram);
+				string command;
+				cin >> command;
 
-			for(uint16_t ram_index = ram_window_start; (ram_index < ram_window_end && (ram_index + 7) < 1024); ram_index += 8) {
-				printf("%03x: ", ram_index);
-				for(int i = 0; i < 8; i++) {
-					printf("%04x", ram[ram_index + i]);
+				switch(command[0]) {
+					default:
+						break;
+					case 'q':
+					case 'Q':
+						quit = 1;
+						break;
+					case '0':
+						ram_window_start = strtol(command.c_str(), NULL, 16);
+						ram_window_end = ram_window_start + 0x80;
+						break;
+					case 'n':
+						next = 1;
+						break;
 				}
+
 				printf("\n");
 			}
-
-			printf("---------------------------------------\n");
-			printf("(n)ext, (q)uit, move to ram offset: ");
-
-			string command;
-			cin >> command;
-
-			switch(command[0]) {
-				default:
-					break;
-				case 'q':
-				case 'Q':
-					quit = 1;
-					break;
-				case '0':
-					ram_window_start = strtol(command.c_str(), NULL, 16);
-					ram_window_end = ram_window_start + 0x80;
-			}
-
-			printf("\n");
 		}
 		
 		// Advance time
